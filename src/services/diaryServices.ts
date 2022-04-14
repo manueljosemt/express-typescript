db13ba6e73d3540c5d0687e22a355cf5935bb200
Manuel Michelangelli <manuel.mjmt@gmail.com>
2022-04-14 18:50:20 -0400

    Project configuration

---
+++ src/services/diaryServices.ts
@@ -0,0 +1,29 @@
+import { DiaryEntry, NonSensitiveInfoDiaryEntry, NewDiaryEntry } from '../types'
+import diaryData from './diaries.json'
+
+const diaries: DiaryEntry[] = diaryData as DiaryEntry[]
+
+export const getEntries = (): DiaryEntry[] => diaries
+
+export const findById = (id: number): NonSensitiveInfoDiaryEntry | undefined => {
+  const entry = diaries.find(d => d.id === id)
+  return entry
+}
+
+export const getEntriesWithoutSensitiveInfo = (): NonSensitiveInfoDiaryEntry[] => {
+  return diaries.map(({ id, date, weather, visibility }) => {
+    return {
+      id, date, weather, visibility
+    }
+  })
+}
+
+export const addEntry = (newDiaryEntry: NewDiaryEntry): DiaryEntry => {
+  const newDiary = {
+    id: Math.max(...diaries.map(d => d.id)) + 1,
+    ...newDiaryEntry
+  }
+
+  diaries.push(newDiary)
+  return newDiary
+}
